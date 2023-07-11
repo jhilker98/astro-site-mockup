c47d80f3137044017ddabf69927b295d257f4ac7
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-11 13:59:34 -0400

    added custom fonts to config viewer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,26 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		configViewer: {
+			// ... configViewer Options
+			typographyExample: "The quick brown fox jumps over the lazy dog.",
+			fonts: [
+				"https://fonts.googleapis.com/css2?family=Rubik&display=swap",
+			"https://fonts.googleapis.com/css2?family=Inter&display=swap",
+			"https://fonts.googleapis.com/css2?family=PT+Sans&display=swap'"  
+			],
+			},
+		extend: {
+			fontFamily: {
+				rubik: ['Rubik', ...defaultTheme.fontFamily.sans],
+				inter: ["'Inter'", ...defaultTheme.fontFamily.sans],
+				ptSans: ["'PT Sans'", ...defaultTheme.fontFamily.sans]
+			}
+		},
 	},
 	plugins: [
 		// grid areas plugin
-		
+		require('@savvywombat/tailwindcss-grid-areas'),
+		require('tailwind-scrollbar')
 	],
 }
