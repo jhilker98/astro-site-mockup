da2103f30699e2c458d082448784b35d6e082fee
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-11 12:06:24 -0400

    added tailwind, mdx, and alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), tailwind(), alpinejs()]
+});
