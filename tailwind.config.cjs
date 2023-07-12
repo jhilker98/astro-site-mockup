aa94084ede308bbb2514fb8dc2f304c511e78ce7
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-12 13:04:19 -0400

    added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,22 +2,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+	darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		gridTemplateAreas: {
 			mobile: ['header header header', 'main main main', 'footer footer footer'],
 			desktop: ['sidebar header header', 'sidebar main main', 'sidebar footer footer'],
 		  },
-		  
 		configViewer: {
 			// ... configViewer Options
 			typographyExample: "The quick brown fox jumps over the lazy dog.",
 			fonts: [
-				"https://fonts.googleapis.com/css2?family=Rubik&display=swap",
-			"https://fonts.googleapis.com/css2?family=Inter&display=swap",
-			"https://fonts.googleapis.com/css2?family=PT+Sans&display=swap'"  
+				"https://fonts.googleapis.com/css2?family=IBM+Plex+Serif&display=swap",
+				"https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap",
 			],
-			},
+		},
 		extend: {
 			gridTemplateColumns: {
 				layout: '0.7fr 2.3fr 1fr',
@@ -27,8 +26,10 @@ module.exports = {
 			  },
 			fontFamily: {
 				rubik: ['Rubik', ...defaultTheme.fontFamily.sans],
-				inter: ["'Inter'", ...defaultTheme.fontFamily.sans],
-				ptSans: ["'PT Sans'", ...defaultTheme.fontFamily.sans]
+				ptSans: ["'PT Sans'", ...defaultTheme.fontFamily.sans],
+				ibmPlexSerif: ['"IBM Plex Serif"', ...defaultTheme.fontFamily.serif],
+				tinos: ['"Tinos"', ...defaultTheme.fontFamily.serif],
+				ibmPlexSans: ['"IBM Plex Sans"', ...defaultTheme.fontFamily.sans],
 			},
 			colors: {
 				background: "#fafaf8",
